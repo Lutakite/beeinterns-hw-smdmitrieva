1a372ed2f35303090c81f2c6893cdb3e3c6cb4d3
lutakite <lutakite@gmail.com>
2020-05-15 18:45:27 +0300

    hw20 v1

--- hw20/hw/validate.js
+++ hw20/hw/validate.js
@@ -1,15 +1,19 @@
 function validate(data) {
-    const { login, password, confirmPassword, license } = data;
+    const { login, password, confirmPassword, license, firstName, gender } = data;
 
     if (!login || !password) {
         alert('Укажите логин/пароль');
+    } else if (login == 'beeline' || login == 'beeinterns' || login == 'bee') {
+        alert('Выбранный логин занят');
     } else if (password.length < 6) {
         alert('Пароль должен быть длинной не менее 6 символов');
     } else if (password !== confirmPassword) {
         alert('Пароли должны совпадать');
+    } else if (!firstName) {
+        alert('Укажите имя');
     } else if (!license) {
         alert('Необходимо согласие');
-    } else {
-        alert('Форма отправлена');
+    } else {   
+        alert(`${(gender == 'male') ? 'Уважаемый' : 'Уважаемая'} ${firstName}, заявка создана`);
     }
 }
