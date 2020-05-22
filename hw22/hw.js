1ff94d251ec38843cc8b21710470da614e2ccd07
lutakite <lutakite@gmail.com>
2020-05-22 20:21:00 +0300

    hw22 init

---
+++ hw22/hw.js
@@ -0,0 +1,36 @@
+// Задание 1
+
+let i = 0;
+
+for (??; i <= 50; ??) {
+  console.log(i);
+}
+// Ожидаемый результат: 10 20 30 40 50
+
+// Задание 2
+/*
+ Написать функцию-конструктор Men с ДВУМЯ аргументами
+  С помощью которой можно создать объект
+  { profession: 'hero', name: 'Кларк', secondName: 'Кент' }
+*/
+
+const superman = new Men('Кларк', 'Кент');
+console.log('superman: ', superman);
+// Men { profession: 'hero', name: 'Кларк', secondName: 'Кент' }
+
+
+// Задание 3
+// Напишите функцию sum, которая работает так: sum(a)(b) возвращает a + b.
+sum(1)(2);  // 3
+sum(5)(-1); // 4
+
+
+// Задание 4
+// Измените код так, чтобы console.log выводил последовательно символы 'П т и ц а'
+const bird = ['П', 'т', 'и', 'ц', 'а'];
+
+for (var index = 0; index < bird.length; index++) {
+  setTimeout(function(index) {
+    console.log(bird[index]);
+  }, 100);
+}
